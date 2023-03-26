4e796130f680b074dfdba9d026e8f684e5c3775d
Влада <85355616+VladaKrasnova@users.noreply.github.com>
2023-03-26 15:46:54 +0300

    Homework 2.
    2.2 Переменные. Типы данных

---
+++ script.js
@@ -0,0 +1,70 @@
+    // Задание 1
+// let A = 10;
+// alert(A);
+// A = 20;
+// alert(A);
+
+    // Задание 2
+// let firstIPhone = 2007;
+// alert(firstIPhone);
+
+    // Задание 3
+// let authorJS= 'Brandan Eich';
+// alert(authorJS);
+
+    // Задание 4
+// let x = 10;
+// let y = 2;
+// alert(x + y);
+// alert(x - y);
+// alert(x * y);
+// alert(x / y);
+
+    // Задание 5
+// let result = 2**5
+// alert(result);
+
+    // Задание 6
+// let a = 9;
+// let b = 2;
+// alert(a % b);
+
+    // Задание 7
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num += 1;
+// num -= 1;
+// alert(num);
+
+    // Задание 8
+// let age = prompt("Сколько вам лет?", "");
+// alert(age);
+
+    // Задание 9
+// let user = {
+//     name: 'Vlada',
+//     age: 24,
+//     isAdmin: true
+// }
+
+    // Задание 9.1
+// user['city of residence'] = 'Tula';
+
+    // Задание 9.2
+// user.age = 25;
+
+    // Задание 9.3
+// delete user['city of residence'];
+
+    // Задание 9.4
+// let info = prompt("Какую информацию хотите узнать о пользователе?", "");
+
+// alert(user[info]);
+
+    // Задание 10
+
+let username = prompt("Ваше имя?", "");
+alert(`Привет, ${username} !`);
