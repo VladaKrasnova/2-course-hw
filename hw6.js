deb2ed978e75600aeb28e4c41e8e5b5c8c7cc033
Влада <crasnowa.vlada@yandex.ru>
2023-04-11 14:42:16 +0300

    Выполнение ДЗ

---
+++ hw6.js
@@ -0,0 +1,72 @@
+// УРОК 6
+
+    // Задание 1
+
+// const arr = [1, 5, 4, 10, 0, 3];
+
+// for (let item of arr) {
+//     if (item == 10){
+//         console.log(item);
+//         break;
+//     }
+// 	console.log(item);
+// }
+
+    // Задание 2
+
+// const array = [1, 5, 4, 10, 0, 3];
+
+// console.log(array.indexOf(4));
+
+
+    // Задание 3
+
+// const num = [1, 3, 5, 10, 20];
+
+// console.log(num.join(' '));
+
+
+    // Задание 4
+
+// let arr = [];
+
+// for(let i = 0; i < 3; i++){
+//     arr[i] = [];
+//     for(let k = 1; k < 4; k++){
+//         arr[i].push(k - (k-1));
+//     }
+// }
+
+// console.log(arr);
+
+
+    // Задание 5
+
+// let array = [1, 1, 1];
+
+// array.push(2, 2, 2);
+
+// console.log(array);
+
+
+    // Задание 6
+
+// let arr = [9, 8, 7, 'a', 6, 5];
+
+// arr.sort();
+// arr.pop();
+
+// console.log(arr);
+
+    // Задание 7
+
+// const arr = [9, 8, 7, 6, 5];
+// let num = Number(prompt('Введите число от 1 до 10'));
+
+// if (arr.indexOf(num) >= 0){
+//     alert(`Число ${num} есть в массиве`)
+// }else{
+//     alert(`Число ${num} нет в массиве`)
+// }
+
+
